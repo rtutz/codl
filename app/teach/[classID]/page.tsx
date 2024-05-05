ddfd4caeea348f4f72d1735be5e6bb709ab4efee
ronald <ronaldjohn.tutor@gmail.com>
2024-05-05 19:19:48 -0400

    some more ui for inside class

--- app/teach/[classID]/page.tsx
+++ app/teach/[classID]/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { redirect, useParams } from "next/navigation"
 import { useSession } from "next-auth/react"
+import { ChevronLeftIcon, Pencil1Icon } from "@radix-ui/react-icons"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 function ClassHome() {
     const { data: session, status, update } = useSession()
@@ -10,7 +13,37 @@ function ClassHome() {
     const classID = useParams<{classID: string}>()
     console.log(classID?.classID)
     return (
-        <h1>test</h1>
+        <div>
+            <div className="flex items-center justify-start top-0 left-0 m-10">
+                
+                <ChevronLeftIcon className="h-4 w-4" color="#ADADAD"/>
+                <span className="gray text-sm">Back to all classes</span>
+            </div>
+            {/* header */}
+            <div className="m-10 w-3/4 flex justify-between items-center flex-shrink-0 mx-auto">
+                <div className="w-full h-full flex items-center">
+                    <Avatar className="h-1/6 w-1/6 mr-10">
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col justify-start">
+                    <h1 className="text-3xl">Intro to Python</h1>
+                        <span className="text-xs">6473338512</span>
+                    </div>
+                </div>
+                <div>
+                    <Pencil1Icon/>
+                </div>
+
+            </div>
+
+            {/* Manage Team Members */}
+
+            {/* Button */}
+            <div></div>
+
+            {/* Who's Coding? */}
+        </div>
     )
 }
 
