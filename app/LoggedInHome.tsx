f11f1ee7c14cf1a752a9319292b400bd5562f8d0
ronald <ronaldjohn.tutor@gmail.com>
2024-05-05 15:11:52 -0400

    initial ui for logged in user

--- app/LoggedInHome.tsx
+++ app/LoggedInHome.tsx
@@ -1,18 +1,69 @@
+import { ChevronLeftIcon } from "@radix-ui/react-icons"
+import { Cross1Icon } from "@radix-ui/react-icons"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
 export default function LoggedInHome() {
     return (
         <div>
             {/* Back button */}
-            <div className="absolute top-0 left-0 m-10">
-                <div className="flex">
-                    <p>test</p>
-                    <p>test</p>
-                </div>
+            <div className="top-0 left-0 m-10">
+                <Button>
+                    <ChevronLeftIcon className="h-4 w-4" />
+                    Logout
+                </Button>
             </div>
 
             {/* Div for center materials */}
-            <div>
+            <div className="flex flex-col items-center min-h-screen mt-10  w-full">
 
+                <div  className="w-1/2">
+                    <div className="flex justify-between items-center">
+                        <h1 className="">Classes I'm Teaching</h1>
+                        <Button>Create New Class</Button>
+                    </div>
+                    <Card className="p-5">
+                        <CardContent className="space-y-6 gray">
+                            {/* Would be the individual class */}
+                            <div className="flex justify-between items-center hover:text-white">
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1>Intro to Python</h1>
+                                <Cross1Icon/>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1>Intro to Python</h1>
+                                <Cross1Icon/>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1>Intro to Python</h1>
+                                <Cross1Icon/>
+                            </div>  
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
+
         </div>
     )
 }
