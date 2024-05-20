054ad91a2a0f2189b7b3daa51f7e5501412ca3e2
ronald <ronaldjohn.tutor@gmail.com>
2024-05-20 09:23:32 -0400

    Created the popup for creating a new class

--- app/LoggedInHome.tsx
+++ app/LoggedInHome.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ChevronLeftIcon, Cross1Icon, PlusIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 import {
@@ -9,6 +11,18 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  
 import Link from "next/link"
 
 
@@ -49,7 +63,44 @@ export default function LoggedInHome() {
                 <div  className="w-1/2">
                     <div className="flex justify-between items-center mb-4">
                         <h1 className="font-semibold text-2xl">Classes I'm Teaching</h1>
-                        <Button> <PlusIcon className="h-4 w-4"/>Create New Class</Button>
+
+                         {/* Button for Creating New Class */}
+                         <Dialog>
+                            <DialogTrigger asChild>
+                                <Button> <PlusIcon className="h-4 w-4"/>Create New Class</Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[700px]">
+                                <DialogHeader>
+                                    <DialogTitle>Create New Class</DialogTitle>
+                                    <DialogDescription>
+                                        Create a new class here. You must provide a unique name for this class.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className="flex flex-col space-y-8">
+                                    <div className="flex flex-col space-y-4">
+                                        <Label htmlFor="name">
+                                            Class Display Name
+                                        </Label>
+                                        <Input id="name" value=""/>
+                                    </div>
+
+                                    <div className="flex flex-col space-y-4">
+                                        <Label htmlFor="username">
+                                            Unique Class Identifier
+                                        </Label>
+                                        <Input id="id" value=""/>
+                                    </div>
+                                </div>
+                                <DialogFooter>
+                                    <Button type="submit">Save changes</Button>
+                                </DialogFooter>
+                            </DialogContent>
+                         </Dialog>
+
+
+
+
+
                     </div>
                     <Card className="p-5">
                             <CardContent className="space-y-6 gray">
@@ -75,7 +126,7 @@ export default function LoggedInHome() {
                 <div  className="w-1/2 mt-20">
                     <div className="flex justify-between items-center mb-4">
                         <h1 className="font-semibold text-2xl">Classes I'm Enrolled In</h1>
-                        <Button> <PlusIcon className="h-4 w-4"/>Create New Class</Button>
+                        <Button> <PlusIcon className="h-4 w-4"/>Join New Class</Button>
                     </div>
                     <Card className="p-5">
                             <CardContent className="space-y-6 gray">
