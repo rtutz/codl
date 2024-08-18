da856331dbb1748f47d030fde6cc216139210cc6
ronald <ronaldjohn.tutor@gmail.com>
2024-08-18 13:30:10 -0400

    Added secret on nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,12 +5,14 @@ import prisma from '../../../prisma/client'
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
+  
   callbacks: {
     session({ session, token, user }) {
     session.user.id = user.id; //  Add role value to user object so it is passed along with session            
